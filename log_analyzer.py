184116371b48f6c618b3f920b7a648e053ef59cf
Me11or <91011259+Me11or@users.noreply.github.com>
2021-11-10 21:09:05 +0300

    Доработки по первым замечаниям

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,253 +1,299 @@
-import os
-import re
-import datetime
+import argparse
+import configparser
 import collections
+import datetime
 import gzip
 import json
-import string
-import configparser
 import logging
-import argparse
+import os
+from pathlib import Path
+import re
+from statistics import median
+import string
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
-#                     '$status $body_bytes_sent "$http_referer" '
-#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
-#                     '$request_time';
-
-config = {
-    "REPORT_SIZE": 1000,
-    "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log",
-    "REPORT_TEMPL_PATH": "./report.html",
-    "CRIT_ERR_PERCENT": 30
+CONFIG = {
+    'REPORT_SIZE': 1000,
+    'REPORT_DIR': './reports',
+    'LOG_DIR': './log',
+    'REPORT_TEMPL_PATH': './report_templ.html',
+    'CRIT_ERR_PERCENT': 30,
 }
 
 
-def main(config_file_path):
+def main(config_file_path: str) -> None:
 
     # Получим настройки из файла и смержим с настройками по умолчанию
-    self_log_file, log_dir, rep_dir, rep_size, report_templ_path, crit_err_percent = config_reader(config_file_path, config)
+    config_dict = read_config(config_file_path, CONFIG)
 
     # Настроим логирование
-    logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s',
-                        datefmt='%Y.%m.%d %H:%M:%S',
-                        filename=self_log_file,
-                        filemode='a',
-                        level=logging.INFO)
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+        filename=config_dict['self_log_file'],
+        filemode='a',
+        level=logging.INFO,
+    )
 
-    logging.info("Начинаем обработку")
+    logging.info('Начинаем обработку')
 
-    # Отловим "неожиданные ошибки", чтобы записать их в лог
+    # Отловим 'неожиданные ошибки', чтобы записать их в лог
     try:
         # Проверим, есть ли где искать
-        if os.path.exists(log_dir):
+        log_dir = Path(config_dict['log_dir'])
+        if log_dir.exists():
 
-            # Ищем самый "свежий" лог
-            targetfile = serch_last_file(log_dir)
+            # Ищем самый 'свежий' лог
+            target_file = search_last_file(config_dict['log_dir'])
 
             # Если ничего не нашли, то и делать дальше нечего
-            if targetfile.filename:
+            if target_file.file_name:
 
-                log_path = log_dir+"/"+targetfile.filename
-                target_report_path = rep_dir+"/report-"+targetfile.filedate.strftime('%Y.%m.%d')+".html"
+                log_path = log_dir / target_file.file_name
+                rep_dir = Path(config_dict['rep_dir'])
+                target_report_path = rep_dir / ('report-' + target_file.file_date.strftime('%Y.%m.%d') + '.html')
 
                 # Проверим, нет ли уже сформированного отчета по этому файлу и если есть выходим
-                if not os.path.exists(target_report_path):
+                if not target_report_path.exists():
 
                     # Парсим файл лога, за одно считаем общее количество записей и общую сумму времени
-                    parced_date, all_count, all_sum, errcount = parce_file(log_path)
-                    if all_count == 0:
+                    parsed_data = parse_file(log_path)
+                    if not parsed_data['all_count']:
                         err_percent = 100
                     else:
-                        if errcount > 0:
-                            err_percent = ((errcount/all_count)*100)
+                        if parsed_data['err_count'] > 0:
+                            err_percent = (parsed_data['err_count'] / parsed_data['all_count']) * 100
                         else:
                             err_percent = 0
 
                     # Если превышено относительно количество обработанных с ошибкой записей, пишем в лог и выходим
-                    if err_percent <= crit_err_percent:
+                    if err_percent <= config_dict['crit_err_percent']:
 
                         # Посчитаем все необходимые значения и отсортируем по time_sum
-                        calc_data = calc_and_sort(parced_date, all_count, all_sum)
+                        calc_data = enrich_log_data(parsed_data)
 
                         # Берём первые rep_size записи, формируем в json формат и дампим
-                        dump_json_data = gen_json(calc_data, rep_size)
+                        dump_json_data = generate_json(calc_data, config_dict['rep_size'])
 
                         # Рендерим отчет
-                        report_render(dump_json_data, report_templ_path, target_report_path)
-                        logging.info("Обработка завершена. Результат находится в {}".format(target_report_path))
+                        create_report(dump_json_data, config_dict['report_templ_path'], target_report_path)
+                        logging.info('Обработка завершена. Результат находится в {}'.format(target_report_path))
 
                     else:
-                        logging.info("Процент ошибок {}, что превышает максимальный порог {}. Прерываем выполнение.".format(err_percent, crit_err_percent))
+                        logging.info(
+                            'Процент ошибок {}, что превышает максимальный порог {}. Прерываем выполнение.'.format(
+                                err_percent, config_dict['crit_err_percent']
+                            )
+                        )
                 else:
-                    logging.info("Файл {} уже был обработан ранее, результат в {}".format(targetfile.filename, target_report_path))
+                    logging.info(
+                        'Файл {} уже был обработан ранее, результат в {}'.format(
+                            target_file.file_name, target_report_path
+                        )
+                    )
             else:
-                logging.info("Ни один подходящий файл в директории {} не найден".format(log_dir))
+                logging.info('Ни один подходящий файл в директории {} не найден'.format(config_dict['log_dir']))
         else:
-            logging.info("Директория логов {} не найдена".format(log_dir))
+            logging.info('Директория логов {} не найдена'.format(config_dict['log_dir']))
 
     except Exception:
-        logging.exception("Непредвиденная ошибка, запишем в лог")
+        logging.exception('Непредвиденная ошибка, запишем в лог')
         raise
 
 
-def config_reader(config_file_path, def_config):
+def read_config(config_file_path: str, def_config: dict) -> dict:
+    """
+    Читаем конфиг файл и соединяем в приоритете с дефолтными настройками
+    :param config_file_path: путь до файла настройки
+    :type config_file_path: str
+    :param def_config: дефолтные настройки
+    :type def_config: dict
+    :return: возвращаем словарь с параметрами
+    :rtype: dict
+    """
 
     config_file = configparser.ConfigParser()
     config_file.read(config_file_path)
-
-    if config_file.has_option("Options", "SELF_LOG_FILE"):
-        self_log_file = config_file["Options"]["SELF_LOG_FILE"]
+    res_dict = {}
+    if config_file.has_option('Options', 'SELF_LOG_FILE'):
+        res_dict['self_log_file'] = config_file['Options']['SELF_LOG_FILE']
     else:
-        self_log_file = None
+        res_dict['self_log_file'] = None
 
-    if config_file.has_option("Options", "LOG_DIR"):
-        log_dir = config_file["Options"]["LOG_DIR"]
+    if config_file.has_option('Options', 'LOG_DIR'):
+        res_dict['log_dir'] = config_file['Options']['LOG_DIR']
     else:
-        log_dir = def_config.get("LOG_DIR")
+        res_dict['log_dir'] = def_config.get('LOG_DIR')
 
-    if config_file.has_option("Options", "REPORT_DIR"):
-        rep_dir = config_file["Options"]["REPORT_DIR"]
+    if config_file.has_option('Options', 'REPORT_DIR'):
+        res_dict['rep_dir'] = config_file['Options']['REPORT_DIR']
     else:
-        rep_dir = def_config.get("REPORT_DIR")
+        res_dict['rep_dir'] = def_config.get('REPORT_DIR')
 
-    if config_file.has_option("Options", "REPORT_SIZE"):
-        rep_size = int(config_file["Options"]["REPORT_SIZE"])
+    if config_file.has_option('Options', 'REPORT_SIZE'):
+        res_dict['rep_size'] = int(config_file['Options']['REPORT_SIZE'])
     else:
-        rep_size = def_config.get("REPORT_SIZE")
+        res_dict['rep_size'] = def_config.get('REPORT_SIZE')
 
-    if config_file.has_option("Options", "REPORT_TEMPL_PATH"):
-        report_templ_path = config_file["Options"]["REPORT_TEMPL_PATH"]
+    if config_file.has_option('Options', 'REPORT_TEMPL_PATH'):
+        res_dict['report_templ_path'] = config_file['Options']['REPORT_TEMPL_PATH']
     else:
-        report_templ_path = def_config.get("REPORT_TEMPL_PATH")
+        res_dict['report_templ_path'] = def_config.get('REPORT_TEMPL_PATH')
 
-    if config_file.has_option("Options", "CRIT_ERR_PERCENT"):
-        crit_err_percent = int(config_file["Options"]["CRIT_ERR_PERCENT"])
+    if config_file.has_option('Options', 'CRIT_ERR_PERCENT'):
+        res_dict['crit_err_percent'] = int(config_file['Options']['CRIT_ERR_PERCENT'])
     else:
-        crit_err_percent = def_config.get("CRIT_ERR_PERCENT")
+        res_dict['crit_err_percent'] = def_config.get('CRIT_ERR_PERCENT')
 
-    return self_log_file, log_dir, rep_dir, rep_size, report_templ_path, crit_err_percent
+    return res_dict
 
 
-def serch_last_file(log_dir):
-    lastdate = None
-    lastfilename = None
-    lastfiledata = collections.namedtuple('lastfiledata', ['filename', 'filedate'])
-    re_serch_file = re.compile(r"^nginx-access-ui.log-\d{8}.gz$|^nginx-access-ui.log-\d{8}$")
+def search_last_file(log_dir: str) -> tuple:
+    """
+    Ищет в указанной директории последний файл лога соответствующий шаблону имени
+    :param log_dir: путь до директории с файлами логов
+    :type log_dir: str
+    :return: возвращаем имя и дату найденного файла
+    :rtype: tuple
+    """
+
+    last_date = None
+    last_file_name = None
+    last_file_data = collections.namedtuple('last_file_data', ['file_name', 'file_date'])
+    re_search_file = re.compile(r'^nginx-access-ui.log-\d{8}.gz$|^nginx-access-ui.log-\d{8}$')
     re_get_date = re.compile(r'\d{4}\d{2}\d{2}')
     try:
         for file in os.listdir(log_dir):
-            match_file = re.search(re_serch_file, file)
+            match_file = re_search_file.search(file)
             if match_file:
                 match_date = re.search(re_get_date, file)
                 date = datetime.datetime.strptime(match_date.group(), '%Y%m%d').date()
-                if lastdate is None:
-                    lastdate = date
-                    lastfilename = file
-                if lastdate < date:
-                    lastdate = date
-                    lastfilename = file
-        return lastfiledata(lastfilename, lastdate)
+                if last_date is None:
+                    last_date = date
+                    last_file_name = file
+                if last_date < date:
+                    last_date = date
+                    last_file_name = file
+        return last_file_data(last_file_name, last_date)
     except Exception as error:
-        logging.info("Ошибка поиска файла: {}".format(error))
+        logging.info('Ошибка поиска файла: {}'.format(error))
         return None
 
 
-def parce_file(file_path):
-    count = 0
-    sum = 0
-    errcount = 0
-    data_dict = collections.defaultdict(list)
+def parse_file(file_path: Path) -> dict:
+    """
+    Читаем файл, собирая информацию по времени выполнения в разрезе каждого URL
+    :param file_path: путь до файла который будем анализировать
+    :type file_path: Path
+    :return: возвращаем словарь с параметрами
+    :rtype: dict
+    """
+    all_count = 0
+    all_sum = 0
+    err_count = 0
+    parsed_dict = dict()
 
-    if file_path.endswith(".gz"):
+    if file_path.match('*.gz'):
         file = gzip.open(file_path, 'rb')
     else:
         file = open(file_path, 'rb')
 
-    re_url = re.compile(r"GET (.+?) HTTP")
-    re_timedata = re.compile(r"\d+[.]\d*$")
+    re_url = re.compile(r'GET (.+?) HTTP')
+    re_timedata = re.compile(r'\d+[.]\d*$')
 
     open_file_gen = (st for st in file)
 
-    for item in open_file_gen:
-        count += 1
-        item = item.decode("UTF-8")
+    for all_count, item in enumerate(open_file_gen, start=1):
+        item = item.decode('UTF-8')
         try:
             url = re.search(re_url, item).group(1)
-            timedata = float(re.search(re_timedata, item).group())
-            sum += timedata
-            data_dict[url].append(timedata)
+            time_data = float(re.search(re_timedata, item).group())
+            all_sum += time_data
+            if url not in parsed_dict:
+                parsed_dict[url] = dict(time_list=[])
+            parsed_dict[url]['time_list'].append(time_data)
         except Exception:
-            errcount += 1
-            count -= 1
+            err_count += 1
+            all_count -= 1
     file.close()
-    return data_dict, count, sum, errcount
+    parsed_data = {'parsed_dict': parsed_dict, 'all_count': all_count, 'all_sum': all_sum, 'err_count': err_count}
+    return parsed_data
 
 
-def calc_and_sort(parced_date, all_count, all_sum):
-    calculated_data = []
-    for url_str in parced_date:
-        local_count = len(parced_date[url_str])
-        local_sum = sum(parced_date[url_str])
-        # Запишем посчитанные данные в список списков и отсортируем по сумме
-        calculated_data.append([local_count,                   #count
-                                local_sum/local_count,         #time_avg
-                                max(parced_date[url_str]),     #time_max
-                                local_sum,                     #time_sum
-                                url_str,                       #url
-                                median(parced_date[url_str]),  #time_med
-                                local_sum*100/all_sum,         #time_perc
-                                local_count*100/all_count])    #count_perc
-    calculated_data.sort(key=lambda x: x[3], reverse=True)
+def enrich_log_data(parsed_data: dict) -> list:
+    """
+    Высчитываем и добавляем к словарю данные необходимые для отчёта
+    :param parsed_data: словарь с полученными из файла данными
+    :type parsed_data: dict
+    :return: возвращаем лист готовый к дампу в json
+    :rtype: list
+    """
+
+    for url_str, url_data in parsed_data['parsed_dict'].items():
+        local_count = len(url_data['time_list'])
+        local_sum = sum(url_data['time_list'])
+        # Дополним словарь расчитанными для отчета данными
+        url_data['count'] = local_count
+        url_data['time_avg'] = round(local_sum / local_count, 3)
+        url_data['time_max'] = round(max(url_data['time_list']), 3)
+        url_data['time_sum'] = round(local_sum, 3)
+        url_data['url'] = url_str
+        url_data['time_med'] = round(median(url_data['time_list']), 3)
+        url_data['time_perc'] = round(local_sum * 100 / parsed_data['all_sum'], 3)
+        url_data['count_perc'] = round(local_count * 100 / parsed_data['all_count'], 3)
+        # Удалим поле со списком который нам больше не пригодится
+        del url_data['time_list']
+
+    calculated_data = sorted(parsed_data['parsed_dict'].values(), key=lambda x: x['time_sum'], reverse=True)
+
     return calculated_data
 
 
-def gen_json(data, max_size):
-    json_data = []
-    for rec in data:
-        json_data.append({"count": rec[0],
-                          "time_avg": round(rec[1], 3),
-                          "time_max": round(rec[2], 3),
-                          "time_sum": round(rec[3], 3),
-                          "url": rec[4],
-                          "time_med": round(rec[5], 3),
-                          "time_perc": round(rec[6], 3),
-                          "count_perc": round(rec[7], 3)})
-        if len(json_data) >= max_size:
-            break
-    return json.dumps(json_data)
+def generate_json(data: list, max_size: int) -> str:
+    """
+    Отбираем не более максимального количества записей(указывается в настройках) и дампим json
+    :param data: лист с исходными данными готовыми к дампу в json
+    :type data: list
+    :param max_size: максимальное количество записей
+    :type max_size: int
+    :return: возвращаем json готовый к вставке в отчёт
+    :rtype: str
+    """
+    if max_size < len(data):
+        data = data[0:max_size]
+    return json.dumps(data)
 
 
-def report_render(json_data, report_templ_path, target_report_path):
-    report_template = open(report_templ_path, 'r')
-    readed_templ = string.Template(report_template.read())
-    report_template.close()
+def create_report(json_data: str, report_templ_path: str, target_report_path: Path) -> None:
+    """
+    Рендерим отчет по шаблону с полученными данными и размещаем результат в указанной дирректории
+    :param json_data: json готовый к вставке в отчёт
+    :type json_data: str
+    :param report_templ_path: путь до файла шаблона
+    :type report_templ_path: str
+    :param target_report_path: путь для размещения результирующего отчета
+    :type target_report_path: Path
+    :return: None
+    """
+
+    with open(report_templ_path, 'r') as report_template:
+        readed_templ = string.Template(report_template.read())
+
     rendered_report = readed_templ.safe_substitute(table_json=json_data)
-    result_report = open(target_report_path, 'w')
-    result_report.write(rendered_report)
-    result_report.close()
+
+    with open(target_report_path, 'w') as result_report:
+        result_report.write(rendered_report)
+        result_report.close()
 
 
-def median(list):
-    n = len(list)
-    index = n // 2
-    if n % 2:
-        return sorted(list)[index]
-    return sum(sorted(list)[index - 1:index + 1]) / 2
-
-
-if __name__ == "__main__":
-    #Обработаем полученные параметры(в частности путь до файла настрокйи)
+if __name__ == '__main__':
+    # Обработаем полученные параметры(в частности путь до файла настрокйи)
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--config', type=str, dest='conf_path', default='./config.ini')
     args = arg_parser.parse_args()
 
-    #Если не нашли файл настройки, то как по требованиям взрываемся ошибкой
+    # Если не нашли файл настройки, то как по требованиям взрываемся ошибкой
     if os.path.exists(args.conf_path):
         main(args.conf_path)
     else:
-        raise ValueError("Файл настройки {} не найден".format(args.conf_path))
+        raise ValueError('Файл настройки {} не найден'.format(args.conf_path))
